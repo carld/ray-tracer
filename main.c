a6662f27dc37b97d2d18a8413bf7f77b2f5da34b
Carl <carl.douglas@gmail.com>
2017-06-15 19:37:08 -0400

    First attempt at progressive renderering

--- main.c
+++ main.c
@@ -11,6 +11,30 @@
 #include "material.h"
 #include <float.h>
 
+int sample_counter = 0;
+
+struct material gray_metal = { .albedo.x = 0.8, .albedo.y = 0.8, .albedo.z = 0.8, .scatter = metal_scatter };
+struct material gold_metal = { .albedo.x = 0.8, .albedo.y = 0.6, .albedo.z = 0.2, .scatter = metal_scatter  };
+struct material red_ceramic = { .albedo.x = 0.65, .albedo.y = 0.1, .albedo.z = 0.1, .scatter = lambertian_scatter };
+struct material yellow_ceramic = { .albedo.x = 0.8, .albedo.y = 0.8, .albedo.z = 0.0, .scatter = lambertian_scatter };
+struct material blue_ceramic = { .albedo.x = 0.1, .albedo.y = 0.1, .albedo.z = 0.8, .scatter = lambertian_scatter };
+struct material green_ceramic = { .albedo.x = 0.1, .albedo.y = 0.8, .albedo.z = 0.1, .scatter = lambertian_scatter };
+struct material dielectric = { .ref_idx = 1.5, .scatter = dielectric_scatter };
+
+struct sphere world[] = {
+  { .center.x = 0, .center.y = 0, .center.z = -1, .radius = 0.1, .mat = & blue_ceramic },
+  { .center.x = -1, .center.y = 0, .center.z = -1, .radius = 0.4, .mat = & gold_metal },
+  { .center.x = 0, .center.y = 0, .center.z = 0, .radius = 0.2, .mat = & green_ceramic },
+  { .center.x = 0, .center.y = 0, .center.z = -2, .radius = 0.5, .mat = & gray_metal },
+  { .center.x = 1, .center.y = 0, .center.z = -1, .radius = 0.5, .mat = & dielectric },
+  { .center.x = 1, .center.y = 0, .center.z = -1, .radius = -0.49, .mat = & dielectric },
+
+  { .center.x = 0, .center.y = -100.5, .center.z = -1, .radius = 100, .mat = & red_ceramic  }
+};
+vec3 lookfrom =  { .x = 3, .y = 1, .z = 3 };
+vec3 lookat   =  { .x = 0, .y = 0, .z = -1 };
+camera cam;
+
 vec3 color(ray r, struct sphere *spheres, int nsph, int depth) {
   hit_record rec;
   if (world_hit(spheres, nsph, &r, 0.001, FLT_MAX, &rec)) {
@@ -32,37 +56,19 @@ vec3 color(ray r, struct sphere *spheres, int nsph, int depth) {
   }
 }
 
+float mix(float x, float y, float a) {
+ return x * (1.0 - a) + y * a;
+}
+
 XImage *
-CreateTrueColorImage(Display * display, Visual * visual, unsigned char *image, int width, int height)
+UpdateTrueColorImage(Display * display, Visual * visual, unsigned char *image, int width, int height, int sample_number)
 {
   int i, j, s;
-  unsigned char  *image32 = (unsigned char *)malloc(width * height * 4);
-  unsigned char  *p = image32;
-  int ns = 10;
+  unsigned char  *p = image;
+  int ns = 1; /* number of samples in this pass */
 
-  struct material gray_metal = { .albedo.x = 0.8, .albedo.y = 0.8, .albedo.z = 0.8, .scatter = metal_scatter };
-  struct material gold_metal = { .albedo.x = 0.8, .albedo.y = 0.6, .albedo.z = 0.2, .scatter = metal_scatter  };
-  struct material red_ceramic = { .albedo.x = 0.65, .albedo.y = 0.1, .albedo.z = 0.1, .scatter = lambertian_scatter };
-  struct material yellow_ceramic = { .albedo.x = 0.8, .albedo.y = 0.8, .albedo.z = 0.0, .scatter = lambertian_scatter };
-  struct material blue_ceramic = { .albedo.x = 0.1, .albedo.y = 0.1, .albedo.z = 0.8, .scatter = lambertian_scatter };
-  struct material green_ceramic = { .albedo.x = 0.1, .albedo.y = 0.8, .albedo.z = 0.1, .scatter = lambertian_scatter };
-  struct material dielectric = { .ref_idx = 1.5, .scatter = dielectric_scatter };
-
-  struct sphere world[] = {
-    { .center.x = 0, .center.y = 0, .center.z = -1, .radius = 0.1, .mat = & blue_ceramic },
-    { .center.x = -1, .center.y = 0, .center.z = -1, .radius = 0.4, .mat = & gold_metal },
-    { .center.x = 0, .center.y = 0, .center.z = 0, .radius = 0.2, .mat = & green_ceramic },
-    { .center.x = 0, .center.y = 0, .center.z = -2, .radius = 0.5, .mat = & gray_metal },
-    { .center.x = 1, .center.y = 0, .center.z = -1, .radius = 0.2, .mat = & dielectric },
-    { .center.x = 1, .center.y = 0, .center.z = -1, .radius = -0.15, .mat = & dielectric },
-
-    { .center.x = 0, .center.y = -100.5, .center.z = -1, .radius = 100, .mat = & red_ceramic  }
-  };
-  vec3 lookfrom =  { .x = 3, .y = 1, .z = 3 };
-  vec3 lookat   =  { .x = 0, .y = 0, .z = -1 };
   float dist_to_focus = vec3_length(vec3_subtract_vec(lookfrom, lookat));
   float aperture = 1.0;
-  camera cam;
   camera_pos(
       &cam,
       lookfrom,
@@ -77,7 +83,7 @@ CreateTrueColorImage(Display * display, Visual * visual, unsigned char *image, i
   for (i = 0; i < height; i++) {
     for (j = 0; j < width; j++) {
       vec3 col = { .x = 0, .y = 0, .z = 0 };
-      for (s = 0; s < ns; s++) {
+      for (s = 0 ; s < ns; s++ ) {
         float u = (j + drand48()) / (float) width;
         float v = ((height-i) + drand48()) / (float) height;
         ray r = camera_cast_ray(&cam, u, v);
@@ -86,15 +92,24 @@ CreateTrueColorImage(Display * display, Visual * visual, unsigned char *image, i
       col = vec3_divide_float(col, ns);
       col = (vec3) { .x = sqrtf(col.x), .y = sqrtf(col.y), .z = sqrtf(col.z) };
       col = vec3_multiply_float(col, 255.99);
-      *p++ = (int)col.z;
-      *p++ = (int)col.y;
-      *p++ = (int)col.x;
-      *p++ = 0;
+
+      if (sample_number == 0) {
+        *p++ = (int)col.z;
+        *p++ = (int)col.y;
+        *p++ = (int)col.x;
+        *p++ = 0;
+      } else {
+        *p = mix(*p, col.z, 1.0f / sample_number); p++;
+        *p = mix(*p, col.y, 1.0f / sample_number); p++;
+        *p = mix(*p, col.x, 1.0f / sample_number); p++;
+        *p++ = 0;
+      }
 
       printf("\r%.02f%%", (float)  ((i*width+j) / (float) (height*width) * 100.0) );
       }
     }
-  return XCreateImage(display, visual, 24, ZPixmap, 0, (char*)image32, width, height, 32, 0);
+  printf("\n");
+  return XCreateImage(display, visual, 24, ZPixmap, 0, (char*)image, width, height, 32, 0);
 }
 
 void
@@ -109,8 +124,12 @@ processEvent(Display * display, Window window, XImage * ximage, int width, int h
   case ConfigureNotify:
     width = ev.xconfigure.width;
     height = ev.xconfigure.height;
-    ximage = CreateTrueColorImage(display, DefaultVisual(display, 0), 0, width, height);
+    ximage = UpdateTrueColorImage(display, DefaultVisual(display, 0), 0, width, height, sample_counter);
     XPutImage(display, window, DefaultGC(display, 0), ximage, 0, 0, 0, 0, width, height);
+    break;
+  case KeyPress:
+    if(ev.xkey.keycode == 0x09)
+      exit(0);
     break;
   case ButtonPress:
     exit(0);
@@ -120,8 +139,9 @@ processEvent(Display * display, Window window, XImage * ximage, int width, int h
 int
 main(int argc, char **argv)
 {
-  XImage         *ximage;
+  XImage         *ximage = NULL;
   int    width = 640, height = 320;
+  unsigned char *image32 = (unsigned char *)calloc(width * height * 4, sizeof(unsigned char));
   Display        *display = XOpenDisplay(NULL);
   Visual         *visual = DefaultVisual(display, 0);
   Window    window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0, width, height, 1, 0, 0);
@@ -129,10 +149,17 @@ main(int argc, char **argv)
     fprintf(stderr, "Cannot handle non true color visual ...\n");
     exit(1);
   }
-  XSelectInput(display, window, ButtonPressMask | ExposureMask );
+  XSelectInput(display, window, ButtonPressMask | KeyPressMask | KeyReleaseMask | ExposureMask );
   XMapWindow(display, window);
-  ximage = CreateTrueColorImage(display, visual, 0, width, height);
+
+  ximage = UpdateTrueColorImage(display, visual, image32, width, height, sample_counter);
   while (1) {
+    XEvent ev;
+    ev.type = Expose;
+    ev.xexpose.window = window;
+    XSendEvent(display, window, False, ExposureMask, &ev);
+    XFlush(display);
     processEvent(display, window, ximage, width, height);
+    ximage = UpdateTrueColorImage(display, visual, image32, width, height, sample_counter++);
   }
 }
